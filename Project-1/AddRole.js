093fc3901739d9fe69f7b11a3bf49075434f56ef
Dayana1407 <dayana@tvarana.com>
2015-08-21 22:40:20 +0530

    priyanka reddy commited

--- Project-1/AddRole.js
+++ Project-1/AddRole.js
@@ -2,7 +2,7 @@
  * Module Description
  * 
  * Version    Date            Author           Remarks
- * 1.00       22 Oct 2013    Priyanka       Add Roles to the users & more, for attached list AddRole.csv
+ * 1.00       22 Oct 2013    priyanka raddy      Add Roles to the users & more, for attached list AddRole.csv
 
 1. Set role to Not a subscriber in netsuite
 2. Call the updateRole API and set role = 2 in vBulletin
